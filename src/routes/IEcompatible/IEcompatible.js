f4662003b2e817964ff8673e9668d027ed833438
liweirose <2412852641@qq.com>
2019-03-13 20:02:24 +0800

    Ie兼容模块，导出按钮添加

--- src/routes/IEcompatible/IEcompatible.js
+++ src/routes/IEcompatible/IEcompatible.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FreeScrollBar from 'react-free-scrollbar';
+import {Button} from 'antd';
 import style from './IEcompatible.less';
 import Util from '../../utils/util2';
 
@@ -11,20 +12,22 @@ class page extends React.Component {
     };
   }
   componentDidMount (){
+  }
+  daochu = () => {
     Util.html2image(0,'box','LIWEI.png');
   }
   render() {
-  
     return (
       <div id={'box'} className={style.box}>
+      <Button type="danger" ghost onClick={this.daochu}>导出</Button>
       <ul>
           <li>object.assign方法兼容性问题:安装插件babel-polyfill将es6转为es5</li>
           <li>采用hack方法定义IE版本css样式：\0-\8-\9...</li>
           <li>采用多媒体查询定义IE版本css样式：@media all and (-ms-high-contrast: none), (-ms-high-contrast: active)</li>
       </ul>
-          <div style={{width: '300px', height: '100px',color:'#fff',background:'#000'}}>
+          <div style={{width: '300px', height: '100px',color:'#fff',background:'#000',display: 'inline-block'}}>
               <FreeScrollBar>
-                  <h1>The title</h1>
+                  <h1 style={{color:"#fff"}}>The title</h1>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   orem ipsum dolor sit amet, consectetur adipisicing elit
                   orem ipsum dolor sit amet, consectetur adipisicing elit
